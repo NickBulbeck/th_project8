a873bc1b172ee7c2136d8160abfda4436ab612d0
NickBulbeck <bulbecknick@gmail.com>
2021-05-12 17:26:41 +0100

    Completed step 5

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello, wurrld!");
+const sequelize = require('./models/index.js').sequelize;
+console.log(sequelize.options.storage);
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connected successfully: ", sequelize.config);
+    } catch(error) {
+        console.log("Bah: ", error.message);
+    }
+})();
