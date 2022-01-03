868ee40dc8cea437656fe66f4206f4a87a72019a
NickBulbeck <26549198+NickBulbeck@users.noreply.github.com>
2022-01-03 11:26:53 +0000

    Updated some of the paragraph styling

--- routes/errors.js
+++ routes/errors.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const createError = require('http-errors');
 
 /* GET error test url */
+router.get('/', (req,res,next) => {
+  const error = new Error("Testing the basic '/error' route...");
+  next(error);
+});
+
+
 router.get('/:status', (req, res, next) => {
     const stat = req.params.status;
     console.log("Input status: ", stat);
   if (stat) {
     next(createError(stat,`${stat}-type bother has arisen in a strange manner...`));
   } else {
-    next(createError(401,"Bother has arisen in a strange manner..."));
+    // Not sure how execution would actually reach here... hence the status
+    next(createError(666,"Bother has arisen in a VERY strange manner..."));
   }
+
 });
 
 module.exports = router;
